3d23e6ba0bbaf272d6134770a391f45a0292066d
JanioMartins <83734560+JanioMartins@users.noreply.github.com>
2021-09-04 00:15:08 -0300

    Atual
    Atual

---
+++ Módulo_4/Mod4_Aula15_03_09_2021/BlueFashionRetailer/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using BlueFashionRetailer.API;
+using BlueFashionRetailer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace BlueFashionRetailer.Controllers
+{
+
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [AuthorizeRoles(RoleType.Common, RoleType.Admin)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ApiBaseController
+    {
+        IAuthService _service;
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+
+        /// <summary>
+        /// Cria um usuário para acesso aos registros no banco de dados.
+        /// </summary>
+        /// <param name="identityUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Register")]
+        [AllowAnonymous]
+        public IActionResult Register([FromBody] IdentityUser identityUser)
+        {
+            IdentityResult result = _service.Create(identityUser).Result;
+            identityUser.PasswordHash = null;
+            return result.Succeeded ?
+                ApiOk(identityUser) :
+                ApiBadRequest(result.Errors, "Erro ao criar usuário");
+        }
+
+
+        /// <summary>
+        /// Cria um token de autorização para efetuar login.
+        /// </summary>
+        /// <param name="identityUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Token")]
+        [AllowAnonymous]
+        public IActionResult Token([FromBody] IdentityUser identityUser)
+        {
+            try 
+            {
+                return ApiOk(_service.GenerateToken(identityUser));
+            }
+            catch(Exception e)
+            {
+                return ApiBadRequest(e, e.Message);
+            }
+        }
+    }
+}
---
+++ Módulo_4/Mod4_Aula15_03_09_2021/BlueFashionRetailer/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using BlueFashionRetailer.API;
+using BlueFashionRetailer.Models;
+using BlueFashionRetailer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlueFashionRetailer.Controllers
+{
+    //[Authorize(Roles = "Common")]
+    [AuthorizeRoles(RoleType.Common, RoleType.Admin)]
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ApiBaseController
+    {
+        IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+
+        /// <summary>
+        /// Retorna uma lista de todos os produtos existentes no banco de dados.
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public IActionResult Index() =>
+            ApiOk(_service.All());
+
+
+
+        /// <summary>
+        /// Retorna um produto específico de acordo com o Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult Index(int? id)
+        {
+            Product exists = _service.Get(id);
+            return exists == null?
+                ApiNotFound("Não foi encontrado o produto solicitado.") :
+                ApiOk(exists);
+        }
+
+
+        /// <summary>
+        ///Cria um novo produto no banco de dados.
+        /// </summary>
+        /// <param name="prod"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create([FromBody] Product prod)
+        {
+            prod.createdById = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            return _service.Create(prod) ?
+                ApiOk("Produto criado com sucesso!") :
+                ApiNotFound("Erro ao criar produto!");
+        } 
+
+        /// <summary>
+        /// Atualiza um produto no banco de dados.
+        /// </summary>
+        /// <param name="prod"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update([FromBody] Product prod)
+        {
+            prod.updatedById = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            return _service.Update(prod) ?
+                ApiOk("Produto atualizado com sucesso!") :
+                ApiNotFound("Erro ao atualizar produto!");
+        }
+
+
+        /// <summary>
+        /// Apaga o registro de um produto de acordo com o Id no banco de dados.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [AuthorizeRoles(RoleType.Admin)]
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult Delete(int? id) =>
+            _service.Delete(id) ?
+                ApiOk("Produto deletado com sucesso!") :
+                ApiNotFound("Erro ao deletar produto!");
+
+
+        /// <summary>
+        /// Atribui o usuário resposável pela criação e alteração do registro no banco de dados.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [Route("ProductsByRole/{role?}")]
+        [HttpGet]
+        public IActionResult ProductsByRole(string role)
+        {
+            return ApiOk(_service.ProductsByUserRole(role));
+        }
+    }
+}
